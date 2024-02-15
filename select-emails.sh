d96fde712ad16b9d2b39bd5efa23943e43faa666
olaniyiolufemi <o.olufemi@alustudent.com>
2024-02-15 13:07:18 -0800

    Olaniyi again

--- select-emails.sh
+++ select-emails.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
 view_all_students() {
-    while IFS=, read -r email age id; do
-        echo "Email: $email, Age: $age, ID: $id"
-    done < students-list_1023.txt
+    echo "List of students:"
+    if [ -s "students-list_1023.txt" ]; then
+        cat students-list_1023.txt
+    else
+        echo "No students found."
+    fi
 }
