c4a6fd1244d4fafcc9604f9b5940eb5d379a1855
Clarance Liberiste Ntwari <60586899+claranceliberi@users.noreply.github.com>
2024-02-21 22:52:21 +0200

    feat: backup shells

--- backup-15.sh
+++ backup-15.sh
@@ -12,11 +12,12 @@ remote_dir="/summative/1023-2024j"
 local_dir="negpod_$negpod_id-q1"
 remote_path="$remote_dir/$negpod_id"
 
+
 # Create a temporary archive
 tar -cvzf backup.tar.gz "$local_dir"
 
 # Upload the archive to the remote server using `scp`
-scp -i my_private_key.pem backup.tar.gz "$username@$host:$remote_path"
+scp backup.tar.gz "$username@$host:$remote_path"
 
 # Remove the temporary archive
 rm backup.tar.gz
